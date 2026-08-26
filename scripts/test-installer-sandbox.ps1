f6dd1f65333e977be2659c4b6bdb7de3743361fe
dlfkd <153354744+HechoLP@users.noreply.github.com>
2026-08-26 15:29:24 +0900

    chore(release): prepare v0.2.0-alpha.2 (#35)

--- scripts/test-installer-sandbox.ps1
+++ scripts/test-installer-sandbox.ps1
@@ -2,7 +2,7 @@
 param(
     [Parameter()]
     [ValidatePattern('^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-[0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*)?$')]
-    [string] $Version = '0.1.0-alpha.1',
+    [string] $Version = '0.2.0-alpha.2',
 
     [Parameter()]
     [ValidateRange(2, 30)]
